d408f7801bf5df01b0948d479fa1583f23988457
ikhwnysrh <ikhwanyusrah@gmail.com>
2024-05-09 06:02:37 +0800

    perubahan kecil

--- src/404.jsx
+++ src/404.jsx
@@ -2,10 +2,11 @@ import './App.css';
 const errorPage = () => {
     return (
         <div className="error">
-            <span style={{fontSize: 60}}>☹</span>
-            {/* <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjA0cWlsMjVzeW5ucmx0cWtlaDZuendtNmM1N3lxY3czamVuMGZodyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/KbLg28Z0uhEtcrjLoH/giphy.gif" alt="404"/> */}
-            <h1 className='h1-404'>404</h1>
-            <h3>Page not found</h3>
+            {/* <span style={{fontSize: 60}}>☹</span> */}
+            <img src="./404.png" alt="404" width={"300px"}/>
+            {/* <h1 className='h1-404'>404</h1> */}
+            <h1>Page not found!</h1>
+            <p>The Requested page was not found</p>
         </div>
     )
 }
