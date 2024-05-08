33994b475096e0b09cb050d26642c0ba013915bd
ikhwnysrh <ikhwanyusrah@gmail.com>
2024-05-09 06:05:59 +0800

    perubahan kecil pt2

--- src/404.jsx
+++ src/404.jsx
@@ -5,8 +5,8 @@ const errorPage = () => {
             {/* <span style={{fontSize: 60}}>☹</span> */}
             <img src="./404.png" alt="404" width={"300px"}/>
             {/* <h1 className='h1-404'>404</h1> */}
-            <h1>Page not found!</h1>
-            <p>The Requested page was not found</p>
+            <h1>Sorry, Page not found!</h1>
+            <p>The requested page was not found</p>
         </div>
     )
 }
