a1ba68d7e3b40e4642b1c8feec9079df5da5b817
ikhwnysrh <ikhwanyusrah@gmail.com>
2024-05-25 20:16:51 +0800

    fixing error page

--- src/404.jsx
+++ src/404.jsx
@@ -5,7 +5,7 @@ const errorPage = () => {
             {/* <span style={{fontSize: 60}}>☹</span> */}
             <img src="./404.png" alt="404" width={"300px"}/>
             {/* <h1 className='h1-404'>404</h1> */}
-            <h1>Sorry, Page not found!</h1>
+            <h2>Sorry, Page not found!</h2>
             <p>The requested page was not found</p>
         </div>
     )
